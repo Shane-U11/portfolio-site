dfd5908700adf10e9190bd9dbcdb459f96a8542d
Shane-U11 <shaneulrich@icloud.com>
2024-06-26 07:13:17 -0700

    lastest version

--- source-code/src/app/components/Dashboard.tsx
+++ source-code/src/app/components/Dashboard.tsx
@@ -7,10 +7,10 @@ const Home = () => {
         <div className="App">
             <Parallax pages={2} style={{ top: "0", left: "0" }} className="animation">
                 <ParallaxLayer offset={0} speed={0.25}>
-                    <div className="animation_layer parallax" id="artback"></div>
+                    <div className="animation_layer parallax" id="background"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.3}>
-                    <div className="animation_layer parallax" id="mountain"></div>
+                    <div className="animation_layer parallax" id="mountains"></div>
                 </ParallaxLayer>
 
                 <ParallaxLayer offset={0} speed={0.3}>
@@ -26,7 +26,7 @@ const Home = () => {
                     <div className="animation_layer parallax" id="jungle4"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.4}>
-                    <div className="animation_layer parallax" id="manonmountain"></div>
+                    <div className="animation_layer parallax" id="man_on_mountain"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={0.35}>
                     <div className="animation_layer parallax" id="jungle5"></div>
